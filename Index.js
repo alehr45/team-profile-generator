4dcba4930d00f5e9851cb6ff99f0c8f6010fef13
Your NameAlex <alehr45@gmail.com>
2021-02-20 12:14:57 -0600

    styling for roster

--- Index.js
+++ Index.js
@@ -58,20 +58,24 @@ function createRosterHTML(response) {
     <!DOCTYPE html>
     <html lang="en">
     <head>
+    <link rel="stylesheet" href="style.css">
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>My Custom Roster</title>
     </head>
     <body>
-        <h1 class="manager">Team Manager: ${response.teammanager}</h1>
-        <h1 class="id">Employee ID: ${response.id}</h1>
-        <a class="email" href="mailto:${response.email}">Email: ${response.email}</a>
-        <h1 class= "number">Office Number: ${response.officenumber}</h1>
-       
-        
+    <div>
+    <div class="container">
+    <p class="manager">Team Manager: ${response.teammanager}</p>
+    <p class="id">Employee ID: ${response.id}</p>
+    <p class="officenumber">Office #: ${response.officenumber}</p>  <br>
+    <a class="email" href="mailto:${response.email}">Email: ${response.email}</a>
+    </div>
+    </div>
     </body>
     </html>
+    
     `;
 }
 
